f2ecabfa0c2025e1946673316725ab10f8dc83c4
thijsfranck <thijsfranck@gmail.com>
2022-12-17 20:15:25 +0100

    day 15 part 2

---
+++ thijs/beacon_exclusion_zone/part_2.py
@@ -0,0 +1,179 @@
+import re
+from dataclasses import dataclass
+from os import path
+from typing import Iterable, List, Optional, Set, Tuple
+
+INPUT_PATH = path.join(path.dirname(__file__), "data")
+
+Point = Tuple[int, int]
+Range = Tuple[int, int]
+Segment = Range
+
+Beacon = Point
+Sensor = Point
+
+
+@dataclass
+class RegexParseException(Exception):
+    input_string: str
+# END RegexParseException
+
+
+def manhattan_distance(a: Point, b: Point) -> int:
+    """
+    Calculates the manhattan distance between the given points `a` and `b`.
+    """
+
+    x_a, y_a = a
+    x_b, y_b = b
+
+    return abs(x_a - x_b) + abs(y_a - y_b)
+# END manhattan_distance
+
+
+def read_sensors_and_beacons(path: str) -> Iterable[Tuple[Sensor, Beacon]]:
+    """
+    Reads the input data at the given `path` and yields the position of each `Sensor` and its closest `Beacon`.
+    """
+
+    PATTERN = r"Sensor at x=(?P<sensor_x>-?\d+), y=(?P<sensor_y>-?\d+): closest beacon is at x=(?P<beacon_x>-?\d+), y=(?P<beacon_y>-?\d+)"
+
+    with open(path) as file:
+
+        lines = (line.rstrip("\n") for line in file)
+
+        for line in lines:
+            matches = re.match(
+                pattern=PATTERN,
+                string=line
+            )
+
+            if matches is None:
+                raise RegexParseException(line)
+            # END IF
+
+            sensor_x, sensor_y, beacon_x, beacon_y = matches.groupdict().values()
+
+            yield (int(sensor_x), int(sensor_y)), (int(beacon_x), int(beacon_y))
+        # END LOOP
+    # END WITH file
+# END read_sensors_and_beacons
+
+
+def calculate_exclusion_range(sensor: Sensor, beacon: Beacon, y: int) -> Optional[Range]:
+    """
+    Given a `sensor` and its closest `beacon`, calculates the range on axis `y` where no other beacons can exist.
+    Returns `None` if no points on axis `y` are excluded.
+    """
+
+    sensor_x, sensor_y = sensor
+
+    distance = manhattan_distance(sensor, beacon)
+
+    range = distance - abs(sensor_y - y)
+
+    if range < 0:
+        return None
+    # END IF
+
+    return (sensor_x - range, sensor_x + range)
+# END points_in_range
+
+
+def are_disjoint(a: Range, b: Range) -> bool:
+    """
+    Returns `True` if ranges `a` and `b` do not overlap or touch at either ends.
+    """
+
+    a_start, a_end = a
+    b_start, b_end = b
+
+    return a_start - b_end > 1 or b_start - a_end > 1
+# END are_disjoint
+
+
+def calculate_segments(ranges: Iterable[Range]) -> Set[Segment]:
+    """
+    Merges the given ranges into segments.
+    """
+
+    segments: Set[Segment] = set()
+
+    for range in ranges:
+        range_start, range_end = range
+        new_segments = segments.copy()
+
+        for segment in segments:
+
+            if are_disjoint((range_start, range_end), segment):
+                continue
+            # END IF
+
+            new_segments.discard(segment)
+
+            segment_start, segment_end = segment
+            range_start = min(segment_start, range_start)
+            range_end = max(segment_end, range_end)
+        # END LOOP
+
+        new_segments.add((range_start, range_end))
+        segments = new_segments
+    # END LOOP
+
+    return segments
+# END calculate_segments
+
+
+def count_blocked_positions(segments: Iterable[Segment]) -> int:
+    """
+    Returns the sum of the length of all given `segments`.
+    A segment has a minimum length of `1`.
+    """
+
+    return sum(1 + end - start for start, end in segments)
+# END count_blocked_positions
+
+
+def calculate_tuning_frequency(beacon: Beacon):
+    x, y = beacon
+    return x * 4000000 + y
+# END calculate_tuning_frequency
+
+
+def find_missing_beacon(sensors_and_beacons: List[Tuple[Sensor, Beacon]], limit: int) -> Optional[Beacon]:
+
+    beacon = None 
+
+    for y in range(0, limit + 1):
+
+        ranges = filter(None, (
+            calculate_exclusion_range(sensor, beacon, y)
+            for sensor, beacon
+            in sensors_and_beacons
+        ))
+
+        segments = calculate_segments(ranges)
+
+        if len(segments) > 1:
+            a, _ = sorted(segments)
+            beacon = (a[1] + 1, y)
+            break
+        # END IF
+    # END LOOP
+
+    return beacon
+# END find_missing_beacon
+
+
+if __name__ == "__main__":
+    sensors_and_beacons = list(read_sensors_and_beacons(INPUT_PATH))
+    beacon = find_missing_beacon(sensors_and_beacons, 4000000)
+
+    tuning_frequency = None
+
+    if beacon is not None:
+        tuning_frequency = calculate_tuning_frequency(beacon)
+    # END IF
+
+    print(tuning_frequency)
+# END MAIN
---
+++ thijs/beacon_exclusion_zone/test__part_2.py
@@ -0,0 +1,111 @@
+from os import path
+from typing import List, Tuple
+
+from pytest import fixture
+
+from .part_2 import (Beacon, Sensor, are_disjoint, calculate_exclusion_range,
+                     calculate_segments, calculate_tuning_frequency,
+                     count_blocked_positions, find_missing_beacon,
+                     read_sensors_and_beacons)
+
+
+@fixture
+def sample_data() -> List[Tuple[Sensor, Beacon]]:
+    INPUT_PATH = path.join(path.dirname(__file__), "sample_data")
+    return list(read_sensors_and_beacons(INPUT_PATH))
+# END sample_data
+
+
+@fixture
+def data() -> List[Tuple[Sensor, Beacon]]:
+    INPUT_PATH = path.join(path.dirname(__file__), "data")
+    return list(read_sensors_and_beacons(INPUT_PATH))
+# END data
+
+
+def test__calculate_exclusion_range():
+    range = calculate_exclusion_range((8, 7), (2, 10), 10)
+    assert range == (2, 14)
+
+    range = calculate_exclusion_range((9, 16), (10, 16), 17)
+    assert range == (9, 9)
+
+    range = calculate_exclusion_range((2, 18), (-2, 15), 15)
+    assert range == (-2, 6)
+
+    range = calculate_exclusion_range((4, 9), (4, 5), 12)
+    assert range == (3, 5)
+# END test__calculate_exclusion_range
+
+
+def test__are_disjoint():
+    disjoint = are_disjoint((1, 1), (3, 3))
+    assert disjoint
+
+    disjoint = are_disjoint((1, 1), (2, 2))
+    assert not disjoint
+
+    disjoint = are_disjoint((1, 1), (1, 1))
+    assert not disjoint
+
+    disjoint = are_disjoint((1, 1), (0, 0))
+    assert not disjoint
+
+    disjoint = are_disjoint((1, 1), (-1, -1))
+    assert disjoint
+# END test__are_disjoint
+
+
+def test__calculate_segments():
+    # Segments adjacent
+    range = calculate_segments([(2, 2), (3, 3), (1, 1)])
+    assert range == {(1, 3)}
+
+    range = calculate_segments([(1, 1), (3, 3), (2, 2)])
+    assert range == {(1, 3)}
+
+    # Segments overlap
+    range = calculate_segments([(1, 2), (2, 3)])
+    assert range == {(1, 3)}
+
+    range = calculate_segments([(2, 3), (1, 2)])
+    assert range == {(1, 3)}
+
+    # Segment is subset of another
+    range = calculate_segments([(1, 3), (2, 3)])
+    assert range == {(1, 3)}
+
+    range = calculate_segments([(2, 3), (1, 3)])
+    assert range == {(1, 3)}
+
+    # Segments disjoint
+    range = calculate_segments([(1, 1), (3, 3)])
+    assert range == {(1, 1), (3, 3)}
+
+    range = calculate_segments([(3, 3), (1, 1)])
+    assert range == {(1, 1), (3, 3)}
+# END test__calculate_segments
+
+
+def test__calculate_tuning_frequency_sample_data(sample_data: List[Tuple[Sensor, Beacon]]):
+
+    beacon = find_missing_beacon(sample_data, 20)
+
+    assert beacon == (14, 11)
+
+    tuning_frequency = calculate_tuning_frequency(beacon)
+
+    assert tuning_frequency == 56000011
+# END test__count_blocked_positions_sample_data
+
+
+def test__calculate_tuning_frequency(data: List[Tuple[Sensor, Beacon]]):
+
+    beacon = find_missing_beacon(data, 4000000)
+
+    assert beacon is not None
+
+    tuning_frequency = calculate_tuning_frequency(beacon)
+
+    assert tuning_frequency == 56000011
+# END test__count_blocked_position
